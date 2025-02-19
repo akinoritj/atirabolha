cc1d8d4f851908a440cbd8601fa0e05a8bb28803
Thiago Jose Fonseca de Oliveira <Thiago50570676@edu.df.senac.br>
2025-02-19 17:19:37 -0300

    started2025

---
+++ Assets/Scripts/MovimentoBarra.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Callbacks;
+using UnityEngine;
+
+public class MovimentoBarra : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    public float speed;
+    public float bounceForce = 10f;
+
+    void Start()
+    {
+    rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            rb.transform.Translate(speed * Time.deltaTime, 0, 0);
+        }
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            rb.transform.Translate(-speed * Time.deltaTime, 0, 0);
+        }
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Rasengan"))
+        {
+
+
+            Vector2 contactPoint = collision.contacts[0].point;
+            Vector2 platformCenter = GetComponent<Collider2D>().bounds.center;
+
+
+            float hitFactor = (contactPoint.x - platformCenter.x) / (GetComponent<Collider2D>().bounds.size.x / 2);
+        
+                    Rigidbody2D ballRb = collision.gameObject.GetComponent<Rigidbody2D>();
+            if (ballRb != null)
+            {
+        
+                Vector2 bounceDirection = new Vector2(hitFactor, 1).normalized; 
+                ballRb.velocity = bounceDirection * bounceForce; 
+            }
+        }
+    }
+}
