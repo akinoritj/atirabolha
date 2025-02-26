e4cd29202ca0de216974d42f7ded2d9a3eca5cd7
Thiago Jose Fonseca de Oliveira <Thiago50570676@edu.df.senac.br>
2025-02-26 17:34:43 -0300

    2.0

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
 
 public class Ball : MonoBehaviour
 {
+    private int pontos = 0;
     private Rigidbody2D rb;
     
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+
     }
     
     void OnCollisionEnter2D(Collision2D collision)
@@ -18,5 +26,16 @@ public class Ball : MonoBehaviour
             rb.gravityScale = 0;
         }
         
+    }
+
+    public void ontTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+        pontos++;
+    }
+
+    public int PegaPontos()
+    {
+        return pontos;
     }
 }
