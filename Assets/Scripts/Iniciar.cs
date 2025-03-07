0be8f6cf269f2f429c0ced772dc221745377040d
Thiago Jose Fonseca de Oliveira <Thiago50570676@edu.df.senac.br>
2025-03-07 17:43:26 -0300

    alguma coisa

--- Assets/Scripts/Iniciar.cs
+++ Assets/Scripts/Iniciar.cs
@@ -3,15 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class Iniciar : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     public void Jogar()
     {
         SceneManager.LoadScene("SampleScene");
     }
 
-    // Update is called once per frame
-    public void Sair()
+    public void Quit()
     {
-        
+        Application.Quit();
     }
 }
--- Assets/Scripts/MovimentoBarra.cs
+++ Assets/Scripts/MovimentoBarra.cs
@@ -7,7 +7,6 @@ public class MovimentoBarra : MonoBehaviour
 {
     private Rigidbody2D rb;
     public float speed;
-    // public float bounceForce = 10f;
 
     void Start()
     {
@@ -17,36 +16,7 @@ public class MovimentoBarra : MonoBehaviour
     void Update()
     {
         Move();
-        // if(Input.GetKey(KeyCode.RightArrow))
-        // {
-        //     rb.transform.Translate(speed * Time.deltaTime, 0, 0);
-        // }
-        // if(Input.GetKey(KeyCode.LeftArrow))
-        // {
-        //     rb.transform.Translate(-speed * Time.deltaTime, 0, 0);
-        // }
     }
-    // private void OnCollisionEnter2D(Collision2D collision)
-    // {
-    //     if (collision.gameObject.CompareTag("Rasengan"))
-    //     {
-
-
-    //         Vector2 contactPoint = collision.contacts[0].point;
-    //         Vector2 platformCenter = GetComponent<Collider2D>().bounds.center;
-
-
-    //         float hitFactor = (contactPoint.x - platformCenter.x) / (GetComponent<Collider2D>().bounds.size.x / 2);
-        
-    //                 Rigidbody2D ballRb = collision.gameObject.GetComponent<Rigidbody2D>();
-    //         if (ballRb != null)
-    //         {
-        
-    //             Vector2 bounceDirection = new Vector2(hitFactor, 1).normalized; 
-    //             ballRb.linearVelocity = bounceDirection * bounceForce; 
-    //         }
-    //     }
-    // }
         void Move()
         {
             float moveX = Input.GetAxisRaw("Horizontal");
